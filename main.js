ffa2ffd8f22d2e212a1aafa9224c78b275ba4df0
Emerson Warhman <edwarhman@gmail.com>
2021-11-30 18:35:51 -0400

    TASK-11: change put fetch products instructions in a function

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ let searchBar = document.getElementById("search-bar");
 let categorySelector = document.getElementById("category-selector");
 let submitButton = document.getElementById("submit-button");
 
-fetch("products.json")
-.then(response => {
-    return response.json();
-})
-.then(json => {
-    let products = json;
-    console.log(products[0]);
-})
-.catch(err => {
-    console.log("Fetch problem: " + err.message);
-});
+function fetchProducts() {
+  fetch("products.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let products = json;
+      console.log(products[0]);
+    })
+    .catch((err) => {
+      console.log("Fetch problem: " + err.message);
+    });
+}
 
-submitButton.addEventListener("click", e => {
-    e.preventDefault();
-})
+fetchProducts();
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+});
